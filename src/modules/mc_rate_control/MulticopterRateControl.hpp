1e00612617d4129df73a90cd93009e2b01cf01ec
Silvan Fuhrer <silvan@auterion.com>
2022-12-19 15:36:31 +0100

    Rework landing gear logic
    - remove deprecated actuator_controls[INDEX_LANDING_GEAR]
    - remove dead code in mc rate controller that used to prevent it from being retracted
    on the ground (anyway had no effect as it only affected the actuator_control[LANDING_GEAR]
    which wasn't sent to the control allocation)
    - for VTOLs handle deployment/retraction of landing gear in AUTO  as a MC (retract if
    more than 2m above ground, deploy if WP is a landing WP), plus additionally when transition
    flight task is called (ALTITUDE mode and higher)
    - for FW in AUTO: add logic in FW Position Controller, depending on waypoint type mainly
    - manual landing gear settings always come through (a manual command overrides a previous
    auto command, and vice-versa)
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/mc_rate_control/MulticopterRateControl.hpp
+++ src/modules/mc_rate_control/MulticopterRateControl.hpp
@@ -50,7 +50,6 @@
 #include <uORB/topics/actuator_controls_status.h>
 #include <uORB/topics/battery_status.h>
 #include <uORB/topics/control_allocator_status.h>
-#include <uORB/topics/landing_gear.h>
 #include <uORB/topics/manual_control_setpoint.h>
 #include <uORB/topics/parameter_update.h>
 #include <uORB/topics/rate_ctrl_status.h>
@@ -98,7 +97,6 @@ private:
 
 	uORB::Subscription _battery_status_sub{ORB_ID(battery_status)};
 	uORB::Subscription _control_allocator_status_sub{ORB_ID(control_allocator_status)};
-	uORB::Subscription _landing_gear_sub{ORB_ID(landing_gear)};
 	uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
 	uORB::Subscription _vehicle_control_mode_sub{ORB_ID(vehicle_control_mode)};
 	uORB::Subscription _vehicle_land_detected_sub{ORB_ID(vehicle_land_detected)};
@@ -137,8 +135,6 @@ private:
 
 	float _energy_integration_time{0.0f};
 	float _control_energy[4] {};
-
-	int8_t _landing_gear{landing_gear_s::GEAR_DOWN};
 
 	DEFINE_PARAMETERS(
 		(ParamFloat<px4::params::MC_ROLLRATE_P>) _param_mc_rollrate_p,
