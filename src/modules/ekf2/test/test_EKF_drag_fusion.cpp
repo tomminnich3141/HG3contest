66e0c7075fc720fe8f7f4d5b80a1d54d303bd6a2
bresch <brescianimathieu@gmail.com>
2023-01-03 10:56:25 +0100

    EKF2: add more drag fusion tests

--- src/modules/ekf2/test/test_EKF_drag_fusion.cpp
+++ src/modules/ekf2/test/test_EKF_drag_fusion.cpp
@@ -101,8 +101,8 @@ TEST_F(EkfDragFusionTest, testForwardMomentumDrag)
 
 	// drag acceleration = mcoef * airspeed
 	Vector2f predicted_accel;
-	predicted_accel(0) =   CONSTANTS_ONE_G * asinf(pitch);
-	predicted_accel(1) = - CONSTANTS_ONE_G * asinf(roll);
+	predicted_accel(0) =   CONSTANTS_ONE_G * sinf(pitch);
+	predicted_accel(1) = - CONSTANTS_ONE_G * sinf(roll);
 	Vector2f wind_speed = predicted_accel / mcoef;
 	EXPECT_NEAR(vel_wind_earth(0), wind_speed(0), fmaxf(1.0f, 0.1f * fabsf(wind_speed(0))));
 	EXPECT_NEAR(vel_wind_earth(1), wind_speed(1), fmaxf(1.0f, 0.1f * fabsf(wind_speed(1))));
@@ -140,8 +140,8 @@ TEST_F(EkfDragFusionTest, testLateralMomentumDrag)
 
 	// drag acceleration = mcoef * airspeed
 	Vector2f predicted_accel;
-	predicted_accel(0) =   CONSTANTS_ONE_G * asinf(pitch);
-	predicted_accel(1) = - CONSTANTS_ONE_G * asinf(roll);
+	predicted_accel(0) =   CONSTANTS_ONE_G * sinf(pitch);
+	predicted_accel(1) = - CONSTANTS_ONE_G * sinf(roll);
 	Vector2f wind_speed = predicted_accel / mcoef;
 	EXPECT_NEAR(vel_wind_earth(0), wind_speed(0), fmaxf(1.0f, 0.1f * fabsf(wind_speed(0))));
 	EXPECT_NEAR(vel_wind_earth(1), wind_speed(1), fmaxf(1.0f, 0.1f * fabsf(wind_speed(1))));
@@ -177,7 +177,7 @@ TEST_F(EkfDragFusionTest, testForwardBluffBodyDrag)
 
 	const Vector2f vel_wind_earth = _ekf->getWindVelocity();
 
-	Vector2f predicted_accel(CONSTANTS_ONE_G * asinf(pitch), 0.0f);
+	Vector2f predicted_accel(CONSTANTS_ONE_G * sinf(pitch), 0.0f);
 	const float airspeed = sqrtf((2.0f * bcoef_x * predicted_accel.length()) /
 				     CONSTANTS_AIR_DENSITY_SEA_LEVEL_15C);
 	Vector2f wind_speed(-airspeed, 0.0f);
@@ -217,10 +217,50 @@ TEST_F(EkfDragFusionTest, testLateralBluffBodyDrag)
 
 	const Vector2f vel_wind_earth = _ekf->getWindVelocity();
 
-	Vector2f predicted_accel(0.0f, - CONSTANTS_ONE_G * asinf(roll));
+	Vector2f predicted_accel(0.0f, - CONSTANTS_ONE_G * sinf(roll));
 	const float airspeed = sqrtf((2.0f * bcoef_y * predicted_accel.length()) /
 				     CONSTANTS_AIR_DENSITY_SEA_LEVEL_15C);
 	Vector2f wind_speed(0.0f, -airspeed);
+
+	// The magnitude of error perpendicular to wind is equivalent to the error in the of wind
+	// which is why we use the same threshold for each axis.
+	EXPECT_NEAR(vel_wind_earth(0), wind_speed(0), 1.0f);
+	EXPECT_NEAR(vel_wind_earth(1), wind_speed(1), 1.0f);
+};
+
+TEST_F(EkfDragFusionTest, testDiagonalBluffBodyDrag)
+{
+	const float roll = math::radians(-10.0f);
+	const float pitch = math::radians(-10.0f);
+	const Eulerf euler_angles_sim(roll, pitch, 0.f);
+	const Quatf quat_sim(euler_angles_sim);
+	_sensor_simulator.simulateOrientation(quat_sim);
+
+	_ekf_wrapper.enableGpsFusion();
+	_sensor_simulator.startGps();
+
+	// Apply parameter changes required to do drag fusion wind estimation
+	const float bcoef_x = 50.0f;
+	const float bcoef_y = 50.0f;
+	const float mcoef = 0.0f;
+	_ekf_wrapper.setDragFusionParameters(bcoef_x, bcoef_y, mcoef);
+	_ekf_wrapper.enableDragFusion();
+
+	// simulate a vehicle that is hovering and tilting into wind
+	_ekf->set_in_air_status(true);
+	_ekf->set_vehicle_at_rest(false);
+
+	// Wind estimation is slow when using drag fusion
+	_sensor_simulator.runSeconds(90);
+
+	EXPECT_TRUE(_ekf_wrapper.isWindVelocityEstimated());
+
+	const Vector2f vel_wind_earth = _ekf->getWindVelocity();
+
+	Vector2f predicted_accel = quat_sim.rotateVectorInverse(Vector3f(0.f, 0.f, -CONSTANTS_ONE_G)).xy();
+	const float airspeed = sqrtf((2.0f * bcoef_y * predicted_accel.norm()) /
+				     CONSTANTS_AIR_DENSITY_SEA_LEVEL_15C);
+	Vector2f wind_speed(airspeed * predicted_accel / predicted_accel.norm());
 
 	// The magnitude of error perpendicular to wind is equivalent to the error in the of wind
 	// which is why we use the same threshold for each axis.
